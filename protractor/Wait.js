ff5cbfd02611c8d2009cdeb301e6ca91722b92e4
Marcin Jurkowski <mjurkowski@qmetric.co.uk>
2015-08-06 16:35:17 +0100

    replace custom functions with protractor expected conditions

--- protractor/Wait.js
+++ protractor/Wait.js
@@ -1,26 +1,23 @@
 /* global browser, protractor, element, by */
 'use strict';
 
-
-var valueOf = function(elementFinder, expectedValue) {
-    return function() {
-        return elementFinder.getAttribute('value').then(function(value) {
-            return value === expectedValue;
-        });
-    };
-};
+var ExpectedConditions = protractor.ExpectedConditions;
 
 module.exports = {
     forElementToBeRendered: function(locator, time) {
         var timeToWait = time || 3000;
         return browser.wait(protractor.ExpectedConditions.presenceOf(element(locator)), timeToWait, 'Timed out while waiting for ' + locator.toString() + ' to be rendered');
     },
-    forElementToBeDisplayed: function(locator) {
+    forElementToBeDisplayed: function(locator, time) {
         var timeToWait = time || 3000;
         return browser.wait(protractor.ExpectedConditions.visibilityOf(element(locator)), timeToWait, 'Timed out while waiting for ' + locator.toString() + ' to be displayed');
     },
-    forElementValueToBe: function(elementFinder, expectedValue) {
+    forElementValueToBe: function(elementFinder, expectedValue, time) {
         var timeToWait = time || 3000;
-        return browser.wait(valueOf(elementFinder, expectedValue), timeToWait, 'Timed out while waiting for ' + elementFinder.locator().value + ' value change');
+        return browser.wait(ExpectedConditions.textToBePresentInElementValue(elementFinder, expectedValue), timeToWait, 'Timed out while waiting for ' + elementFinder.locator().value + ' value change');
+    },
+    forElementContentsToBe: function(elementFinder, expectedContents, time) {
+        var timeToWait = time || 3000;
+        return browser.wait(ExpectedConditions.textToBePresentInElementValue(elementFinder, expectedContents), timeToWait, 'Timed out while waiting for ' + elementFinder.locator().value + ' value change');
     }
 };
