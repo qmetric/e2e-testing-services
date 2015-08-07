f606a23ab09beb0205ecdcc6a92b1309b85ccd75
Marcin Jurkowski <mjurkowski@qmetric.co.uk>
2015-08-07 13:30:31 +0100

    always return once the response is set

--- middleware/requestExpectations.js
+++ middleware/requestExpectations.js
@@ -82,7 +82,7 @@ var expectNextRequestMiddleware = function () {
             }
 
             if (expectedNextRequestBody) {
-                isNextRequestExpected(request)
+                return isNextRequestExpected(request)
                     .then(function() {
                         next();
                     })
